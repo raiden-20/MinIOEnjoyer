40bffdc62e5c26d5b38a96c2fd65c91870e5f784
undine_20 <lena_sheina@bk.ru>
2023-12-11 18:41:05 +0300

    some changes

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ const buttonGet = document.getElementById('get');
 const buttonPost = document.getElementById('post');
 const buttonPost2 = document.getElementById('post2');
 
-let photo = '' // здесь хранится url, который я сама генерирую на фото
-let idPhoto = '' // а здесь уже, который получаю от тебя
+let photo = ''
+let urlPhoto = ''
 
 buttonPost.addEventListener("change", function(event) {
-    debugger
     console.log("загрузить тык");
 
     const file = event.target.files[0];
@@ -21,20 +20,18 @@ buttonPost.addEventListener("change", function(event) {
     )
 
     photo = event.target.files[0]
-    console.log(photo)
 });
 
 buttonPost2.addEventListener("click", function() {
-    console.log('fgvwsvsw')
-    debugger
+    console.log("отправить тык");
     let formData = new FormData()
-    formData.append('url', photo)
-    axios.post('http://localhost:8080/api/photo/send', formData).then(response => {
-        switch (response.status) { // в response.status хранится код
+    formData.append('file', photo)
+    axios.post('http://localhost:8080/api/file/send', formData).then(response => {
+        switch (response.status) { 
             case 200 : {
-                idPhoto = response.data // в response.data приходит айди/юрл (хз что у тебя будет) фото, то есть ты мне возвращаешь только айди/юрл
-                localStorage.setItem('idPhoto', response.data) // эт я добавляю в локальное хранилище браузера, ты не обращай внимание
-                document.getElementById('floatingInput').innerHTML = idPhoto // а эт я сразу в инпут "URL пикчи" отправляю полученный от тебя url/id
+                urlPhoto = response.data 
+                localStorage.setItem('url', response.data)
+                document.getElementById('floatingInput').value = urlPhoto
             }
         }
     })
@@ -42,11 +39,11 @@ buttonPost2.addEventListener("click", function() {
 
 buttonGet.addEventListener("click", function() {
     console.log("получить тык");
-    axios.get(`http://localhost:8080/api/photo/${localStorage.getItem('idPhoto')}`) // а эт вот как раз беру из локального хранилища данные
+    axios.get(`http://localhost:8080/api/photo/${localStorage.getItem('urlPhoto')}`)
         .then(response => {
             switch (response.status) {
                 case 200 :{
-                    document.getElementById('setPhotoOnUrl').setAttribute('src', response.data) // в response.data приходит хзчто?? на саму фотку от тебя, чтобы она отрисовалась и мы сразу отправляем это в src фльл
+                    document.getElementById('setPhotoOnUrl').setAttribute('src', response.data)
                 }
             }
         })
