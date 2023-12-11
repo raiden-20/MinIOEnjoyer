46968036b8e44b311649ecbaf3cd859081346fb8
undine_20 <lena_sheina@bk.ru>
2023-12-11 18:40:43 +0300

    add minio config file

---
+++ src/main/java/ru/vsu/cs/minioenjoyer/service/MinIOConnection.java
@@ -0,0 +1,30 @@
+package ru.vsu.cs.minioenjoyer.service;
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.vsu.cs.minioenjoyer.configuration.MinioBucket;
+
+@Service
+@RequiredArgsConstructor
+public class MinIOConnection {
+
+    private final MinioClient minioClient;
+
+    public String saveFile(MultipartFile file){
+        try {
+            minioClient.putObject(PutObjectArgs
+                    .builder()
+                    .bucket(MinioBucket.PICTURE.toString())
+                    .object(file.getOriginalFilename())
+                    .stream(file.getInputStream(), file.getSize(), 5 * 1024 * 1024)
+                    .build());
+            return MinioBucket.PICTURE.toString() + "/" + file.getOriginalFilename();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
